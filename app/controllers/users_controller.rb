18a4a23edc32dabab6c22c83e9f9dfd01f7905d3
jbrown34 <jbrown34@chicagobooth.edu>
2020-11-03 17:52:51 +0000

    user page mocked up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,17 @@ class UsersController < ApplicationController
     render({:template => "user_templates/index.html.erb"})
   end
 
-  def user_page
-    matching_users = User.all
-
-    @list_of_users = matching_users.order({ :username => :asc}) 
-
-    render({:template => "user_templates/index.html.erb"})
+  def show
+    @url_username = params.fetch("path_username")
+    matching_usernames =  User.where({ :username => @url_username})
+    #can also do matching_usernames.at(0)
+    @the_user = matching_usernames.first
+    #the if function is defensive and doesn't need to be required for all
+    if @the_user == nil
+      redirect_to("/404")
+    else 
+      render({:template => "user_templates/show.html.erb"})
+    end 
   end
   
 end 
